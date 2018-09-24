d78197703e82dbae1ac3c70eb3f1ff99f23b8c4a
Rakib Hassan <rhassan0221@gmail.com>
2018-09-24 13:37:30 -0400

    Search design added

--- frontend/src/Components/Search/Search.js
+++ frontend/src/Components/Search/Search.js
@@ -55,25 +55,17 @@ class Search extends Component {
 	render(){
 		return(
 			<div className="Search">
-				<h3>WHAT'S WRONG?</h3>
-				<input className="text" type="text" name="searchKey" placeholder="Type in body part..." onChange={this.handleSearchKey.bind(this)} value={this.state.data.searchKey}/> <br/>
-				<select onChange={this.handleGender.bind(this)} value={this.state.data.gender}>
-				  	<option value="" selected disabled hidden id="defaultValue">Gender</option>
-				  	<option value="Male">Male</option>
-  					<option value="Female">Female</option>
-				</select><br/>
-				<select onChange={this.handleAge.bind(this)} value={this.state.data.age}>
-				  	<option value="" selected disabled hidden id="defaultValue">Age</option>
-				  	<option value="1">1</option>
-  					<option value="2">2</option>
-  					<option value="3">3</option>
-  					<option value="4">4</option>
-  					<option value="5">5</option>
-  					<option value="6">6</option>
-  					<option value="7">7</option>
-  					<option value="8">8</option>
-				</select><br/>
-				<input type="submit" value="Search" onClick={this.handleSubmit.bind(this)} /> <br/>
+				<h1 id="search-title">WHAT'S WRONG?</h1>
+				<input className="text" id="search-input" type="text" placeholder="Type in body part..." onChange={this.handleSearchKey.bind(this)} value={this.state.data.searchKey}/> <br/>
+				<div id="gender-age">
+					<select id="gender-input" onChange={this.handleGender.bind(this)} value={this.state.data.gender}>
+					  	<option value="" selected disabled hidden id="defaultValue">Gender</option>
+					  	<option value="Male">Male</option>
+	  					<option value="Female">Female</option>
+					</select><br/>
+					<input className="text" id="age-input" type="text" placeholder="Age" onChange={this.handleAge.bind(this)} value={this.state.data.age} />
+				</div>
+				<input id="submit-button" type="submit" value="Search" onClick={this.handleSubmit.bind(this)} /> <br/>
 			</div>
 		);
 	}
