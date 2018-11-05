7f6472f49d71e01c00568008b533cb23a29e8e9b
Rakib Hassan <rhassan0221@gmail.com>
2018-11-05 01:13:41 -0500

    Minimization working

--- frontend/src/Components/Search/Search.js
+++ frontend/src/Components/Search/Search.js
@@ -9,8 +9,13 @@ class Search extends Component {
 
 		this.state = {
 			title: false,
+			titleView: "originalTitleView",
 			input: false,
+			inputView: "originalInputView",
 			genderAge: false,
+			genderAgeView: "originalGenderAgeView",
+			searchButtonView: "originalSearchButtonView",
+			symptomsView: "emptySymptoms",
 			data: {
 				searchKey: '',
 				gender: '',
@@ -64,14 +69,25 @@ class Search extends Component {
 		if(this.state.data.symptoms.length > 0) {
 			this.setState({
 				title: true,
+				titleView: "minimizedTitleView",
 				input: true,
+				inputView: "minimizedInputView",
 				genderAge: true,
+				genderAgeView: "minimizedGenderAgeView",
+				searchButtonView: "minimizedSearchButtonView",
+				symptomsView: "showingSymptoms",
+
 			})
 		} else {
 			this.setState({
 				title: false,
+				titleView: "originialTitleView",
 				input: false,
+				inputView: "originalInputView",
 				genderAge: false,
+				genderAgeView: "originalGenderAgeView",
+				searchButtonView: "originalSearchButtonView",
+				symptomsView: "emptySymptoms",
 			})
 		}
 	}
@@ -83,6 +99,14 @@ class Search extends Component {
 			}
 		})
 	}
+
+	// handleBool() {
+	// 	if(this.state.data.title){
+	// 		this.setState({
+	// 			titleView: "Symptoms",
+	// 		})
+	// 	}
+	// }
 
 	handleSubmit(event) {
 		event.preventDefault();
@@ -128,6 +152,7 @@ class Search extends Component {
 				}
 
 				this.handleMinimize();
+				// this.handleBool();
 
 				console.log(this.state);
 			})
@@ -141,9 +166,9 @@ class Search extends Component {
 	render(){
 		return(
 			<div className="Search">
-				<h1 id="search-title">WHAT'S WRONG?</h1>
-				<input className="text" id="search-input" type="text" placeholder="Type in body part..." onChange={this.handleSearchKey.bind(this)} value={this.state.data.searchKey}/> <br/>
-				<div id="gender-age">
+				<h1 id={this.state.titleView}>WHAT'S WRONG?</h1>
+				<input className="text" id={this.state.inputView} type="text" placeholder="Type in body part..." onChange={this.handleSearchKey.bind(this)} value={this.state.data.searchKey}/> <br/>
+				<div id={this.state.genderAgeView}>
 					<select id="gender-input" onChange={this.handleGender.bind(this)} value={this.state.data.gender}>
 					  	<option value="" selected disabled hidden id="defaultValue">Gender</option>
 					  	<option value="Male">Male</option>
@@ -151,13 +176,29 @@ class Search extends Component {
 					</select><br/>
 					<input className="text" id="age-input" type="text" placeholder="Age" onChange={this.handleAge.bind(this)} value={this.state.data.age} />
 				</div>
-				<input id="search-button" type="submit" value="SEARCH" onClick={this.handleSubmit.bind(this)} /> <br/>
+				<input id={this.state.searchButtonView} type="submit" value="SEARCH" onClick={this.handleSubmit.bind(this)} /> <br/>
 				<div>
 					<ul>
 						{this.state.data.symptoms.map((item,index) => 
 							<li key={index}>{item}</li>
 						)}
 					</ul>
+				</div>
+				<div id={this.state.symptomsView}>
+					<h3 id="apply-title">Check all that apply</h3>
+					<div id="symptoms-list">
+						<input class="symptom" type="checkbox" name="symptom1" value="symptom1" /> Symptom 1<br/>
+						<input class="symptom" type="checkbox" name="symptom2" value="symptom2" /> Symptom 2<br/>
+						<input class="symptom" type="checkbox" name="symptom3" value="symptom3" /> Symptom 3<br/>
+						<input class="symptom" type="checkbox" name="symptom4" value="symptom4" /> Symptom 4<br/>
+						<input class="symptom" type="checkbox" name="symptom5" value="symptom5" /> Symptom 5<br/>
+						<input class="symptom" type="checkbox" name="symptom6" value="symptom6" /> Symptom 6<br/>
+						<input class="symptom" type="checkbox" name="symptom7" value="symptom7" /> Symptom 7<br/>
+						<input class="symptom" type="checkbox" name="symptom8" value="symptom8" /> Symptom 8<br/>
+						<input class="symptom" type="checkbox" name="symptom9" value="symptom9" /> Symptom 9<br/>
+						<input class="symptom" type="checkbox" name="symptom10" value="symptom10" /> Symptom 10<br/>
+					</div>
+					<input id="submit-button" type="submit" value="Submit" onClick={this.handleSubmit.bind(this)} /> <br/>
 				</div>
 				<button><Link to="/symptoms">Symptoms Page</Link></button>
 			</div>
