7764ed2181857385e42594ddf5db1898d97e6502
Rakib Hassan <rhassan0221@gmail.com>
2018-11-05 11:28:15 -0500

    Symptoms showing

--- frontend/src/Components/Search/Search.js
+++ frontend/src/Components/Search/Search.js
@@ -177,6 +177,7 @@ class Search extends Component {
 					<input className="text" id="age-input" type="text" placeholder="Age" onChange={this.handleAge.bind(this)} value={this.state.data.age} />
 				</div>
 				<input id={this.state.searchButtonView} type="submit" value="SEARCH" onClick={this.handleSubmit.bind(this)} /> <br/>
+				{/*
 				<div>
 					<ul>
 						{this.state.data.symptoms.map((item,index) => 
@@ -184,9 +185,11 @@ class Search extends Component {
 						)}
 					</ul>
 				</div>
+				*/}
 				<div id={this.state.symptomsView}>
 					<h3 id="apply-title">Check all that apply</h3>
 					<div id="symptoms-list">
+						{/* 
 						<input class="symptom" type="checkbox" name="symptom1" value="symptom1" /> Symptom 1<br/>
 						<input class="symptom" type="checkbox" name="symptom2" value="symptom2" /> Symptom 2<br/>
 						<input class="symptom" type="checkbox" name="symptom3" value="symptom3" /> Symptom 3<br/>
@@ -197,6 +200,12 @@ class Search extends Component {
 						<input class="symptom" type="checkbox" name="symptom8" value="symptom8" /> Symptom 8<br/>
 						<input class="symptom" type="checkbox" name="symptom9" value="symptom9" /> Symptom 9<br/>
 						<input class="symptom" type="checkbox" name="symptom10" value="symptom10" /> Symptom 10<br/>
+						*/}
+						<ul>
+							{this.state.data.symptoms.map((item,index) => 
+								<li class="symptom" key={index}><input class="checkbox" type="checkbox"/>{item}</li>
+							)}
+						</ul>
 					</div>
 					<input id="submit-button" type="submit" value="Submit" onClick={this.handleSubmit.bind(this)} /> <br/>
 				</div>
