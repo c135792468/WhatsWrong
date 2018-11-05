a2b6c5c6aaf9cf15219f738d15b8aba245ce8e4e
Rakib Hassan <rhassan0221@gmail.com>
2018-11-05 13:33:55 -0500

    Deleted unnecessary lines

--- frontend/src/Components/Search/Search.js
+++ frontend/src/Components/Search/Search.js
@@ -100,14 +100,6 @@ class Search extends Component {
 		})
 	}
 
-	// handleBool() {
-	// 	if(this.state.data.title){
-	// 		this.setState({
-	// 			titleView: "Symptoms",
-	// 		})
-	// 	}
-	// }
-
 	handleSubmit(event) {
 		event.preventDefault();
 
@@ -117,9 +109,6 @@ class Search extends Component {
 				symptoms: []
 			}	
 		})
-		// console.log("Search Key: " + this.state.data.searchKey);
-		// console.log("Gender: " + this.state.data.gender);
-		// console.log("Age: " + this.state.data.age);
 
 		var arr = [];
 		var arrString = [];
@@ -152,7 +141,6 @@ class Search extends Component {
 				}
 
 				this.handleMinimize();
-				// this.handleBool();
 
 				console.log(this.state);
 			})
@@ -177,30 +165,10 @@ class Search extends Component {
 					<input className="text" id="age-input" type="text" placeholder="Age" onChange={this.handleAge.bind(this)} value={this.state.data.age} />
 				</div>
 				<input id={this.state.searchButtonView} type="submit" value="SEARCH" onClick={this.handleSubmit.bind(this)} /> <br/>
-				{/*
-				<div>
-					<ul>
-						{this.state.data.symptoms.map((item,index) => 
-							<li key={index}>{item}</li>
-						)}
-					</ul>
-				</div>
-				*/}
 				<div id={this.state.symptomsView}>
 					<h3 id="apply-title">Check all that apply</h3>
 					<div id="symptoms-list">
-						{/* 
-						<input class="symptom" type="checkbox" name="symptom1" value="symptom1" /> Symptom 1<br/>
-						<input class="symptom" type="checkbox" name="symptom2" value="symptom2" /> Symptom 2<br/>
-						<input class="symptom" type="checkbox" name="symptom3" value="symptom3" /> Symptom 3<br/>
-						<input class="symptom" type="checkbox" name="symptom4" value="symptom4" /> Symptom 4<br/>
-						<input class="symptom" type="checkbox" name="symptom5" value="symptom5" /> Symptom 5<br/>
-						<input class="symptom" type="checkbox" name="symptom6" value="symptom6" /> Symptom 6<br/>
-						<input class="symptom" type="checkbox" name="symptom7" value="symptom7" /> Symptom 7<br/>
-						<input class="symptom" type="checkbox" name="symptom8" value="symptom8" /> Symptom 8<br/>
-						<input class="symptom" type="checkbox" name="symptom9" value="symptom9" /> Symptom 9<br/>
-						<input class="symptom" type="checkbox" name="symptom10" value="symptom10" /> Symptom 10<br/>
-						*/}
+						{/*  <input class="symptom" type="checkbox" name="symptom1" value="symptom1" /> Symptom 1<br/> */}
 						<ul>
 							{this.state.data.symptoms.map((item,index) => 
 								<li class="symptom" key={index}><input class="checkbox" type="checkbox"/>{item}</li>
