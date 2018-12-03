29f34294b547cc1722391a39794152bff67131c1
Rakib Hassan <rhassan0221@gmail.com>
2018-12-03 16:00:32 -0500

    Modal working

--- frontend/src/Components/Search/Search.js
+++ frontend/src/Components/Search/Search.js
@@ -308,24 +308,28 @@ class Search extends Component {
 	render(){
 		return(
 			<div className="Search">
-
+				<button data-toggle="modal" data-target="#myModal" id="modalButton">How To Use</button>
 				<h1 id={this.state.titleView}>WHAT'S WRONG?</h1>
 
-				<div className="Modal">
-					<button data-toggle="modal" data-target="#myModal" id="modalButton">How To Use</button>
-
-					<div id="myModal" className="modal fade" role="dialog">
-			          	<div className="modal-dialog">
-			           		<div className="modal-content">
-			              		<div className="modal-body">
-			                  		<h4>Create Event</h4>
-			                  		<input type="submit" value="Close" data-dismiss="modal"/>
-			              		</div>
-			            	</div>
-			          	</div>
+				<div id="myModal" className="modal fade" role="dialog">
+			        <div className="modal-dialog">
+			        	<div className="modal-content">
+			            	<div className="modal-body">
+			                  	<h3>How to Use WhatsWrong?</h3>
+			                  	<h4>For simple search:</h4>
+			                  	<ol>
+			                  		<li>Enter a simple body part. Select your gender and enter age.</li>
+				                  	<img src="simp-search1.png" />
+			                  		<li>Select the symptoms that apply to you.</li>
+			                  		<img src="simp-search2.png" />
+			                  		<li>See the possible diagnoses along with percentage and advice.</li>
+			                  		<img src="simp-search3.png" />
+			                  	</ol>
+			                  	<button type="submit" data-dismiss="modal" id="closeButton">Close</button>
+			              	</div>
+			            </div>
 			        </div>
-
-		        </div>
+			    </div>
 
 		        <br/>
 
